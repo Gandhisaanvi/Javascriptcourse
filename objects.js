28e31038a86ca5f129e7b17b38323ea55f1854d6
gandhisaanvi <gandhisaanvi7@gmail.com>
2023-12-10 19:13:35 +0530

    commit

---
+++ objects.js
@@ -0,0 +1,16 @@
+var iphone = 
+{
+   Modelno: "iphone 13 pro Max",
+   iphoneprice:"60000inrupees",
+   frontcamera:"12MP (f/1.5)",
+   rearcamera:"12MP (f/2.2)",
+   color:"rose pink",
+   speakers:" dolby atmos 2 speakers",
+   battery:"4352mAH",
+   processor:"A15 Bionic processor"
+
+
+}
+console.log(iphone);
+console.log(iphone.Modelno)//in iphone modelno is the object
+console.table(iphone)//table craeted using this data
