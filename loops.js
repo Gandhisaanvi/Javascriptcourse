a8a537215770fabda20269b25274fd11d61592e0
gandhisaanvi <gandhisaanvi7@gmail.com>
2023-12-13 22:37:53 +0530

    loops

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ const states=["varanasi","delhi","uttrakhand","bangalore","ranchi",1233,907];
 for(let i=0;i<=states.length;i++)//FOR LOOP
 {
     if(typeof states[i]!=="string")continue;//typeof 
-    console.log(states[i])
+   console.log(states[i])
 }
 
 let j=0;//WHILE LOOPS
@@ -13,9 +13,11 @@ while(j<states.length)
     console.log(states[j])
     j++;
 }
-
+ 
 let s=37;//DO WHILE LOOP
 do{
-    console.log(s);
+   console.log(s);
     s++;
 }while(s<states.length)
+
+states.forEach((s)=>(console.log(s)))//for each loop
